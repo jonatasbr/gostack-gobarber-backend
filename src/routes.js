af7196ad12afe7d66d468e156d348c486872bc71
Braz <jonatasbraz@gmail.com>
2019-06-30 13:29:21 -0300

    feature: Config do Eslint, Prettier e EditConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => {
-  return res.json({ message: "hello, rocketseat" });
+routes.get('/teste', (req, res) => {
+    return res.json({ message: 'hello, rocketseat' });
 });
 
 export default routes;
