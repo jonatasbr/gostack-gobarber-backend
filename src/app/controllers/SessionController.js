ff32bf1145177166cdeeaf68ae67618a2762c539
Jonatas Braz <jonatasbraz@gmail.com>
2019-07-09 23:48:36 -0300

    renomear arquivo e suas imports

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
-import authConfig from '../../config/authConfig';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
