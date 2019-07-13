844dcb8044371d3ec2719d78805cfd1c71112f1c
Jonatas Braz <jonatasbraz@gmail.com>
2019-07-13 01:33:02 -0300

    listagem deproviders

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,22 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
-    this.connection = new Sequelize(databaseConfig);
-
     this.init();
   }
 
   init() {
-    models.map(model => model.init(this.connection));
+    this.connection = new Sequelize(databaseConfig);
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
