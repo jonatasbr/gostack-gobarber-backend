9c8423849a1f0057951cd710fbd5f4ffaa4684e7
Jonatas Braz <jonatasbraz@gmail.com>
2019-07-16 01:54:13 -0300

    feature: filas de emails, template de email, etc

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2431fb2f8c26c8',
+    pass: '0f1279f0546f37',
+  },
+  default: {
+    from: 'Team GoBarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/config/redis.js
@@ -0,0 +1,4 @@
+export default {
+  host: '127.9.9.62',
+  port: 6379,
+};
